a8059f15e9d82630cedf524bda2c092a68ed91f9
rahulas2255 <rahulayyappath@gmail.com>
2024-03-04 12:36:00 +0530

    first commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { WishlistComponent } from './wishlist/wishlist.component';
+import { AllProductsComponent } from './all-products/all-products.component';
+import { CartComponent } from './cart/cart.component';
+import { ViewProductComponent } from './view-product/view-product.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    LoginComponent,
+    RegisterComponent,
+    WishlistComponent,
+    AllProductsComponent,
+    CartComponent,
+    ViewProductComponent
   ],
   imports: [
     BrowserModule,
